3fb885d0bf5a74d9663ab09688f0000e76b390b7
Fernando <ferzim@gmail.com>
2020-10-31 18:55:30 -0300

    feat: simple app (TodoApp) with React

---
+++ src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa história</h2>
+        <p>haashaoishsoa hoisa hoi</p>
+        <h2>Missão e visão</h2>
+        <p>h siohaio hioiho h ioh ioho</p>
+        <h2>Imprensa</h2>
+        <p>kkkkk k k k k kk k </p>
+    </div>
+)
